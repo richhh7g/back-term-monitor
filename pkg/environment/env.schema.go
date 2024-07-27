d3b55f751e7716ec3bcc5ab1ab2386b9c19b426c
richhh7g <richhh7g@protonmail.com>
2024-07-27 14:23:54 -0300

    chore: add mongo env validation

--- pkg/environment/env.schema.go
+++ pkg/environment/env.schema.go
@@ -3,5 +3,11 @@ package environment
 var envSchema EnvSchema
 
 type EnvSchema struct {
-	APP_ENV string `validate:"required"`
+	APP_ENV    string `validate:"required"`
+	MONGO_DB   string `validate:"required"`
+	MONGO_URL  string `validate:"required"`
+	MONGO_HOST string `validate:"required"`
+	MONGO_PORT string `validate:"required"`
+	MONGO_USER string `validate:"required"`
+	MONGO_PASS string `validate:"required"`
 }
