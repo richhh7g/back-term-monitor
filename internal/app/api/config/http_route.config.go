57bd25d29661f496422410260fb055a0681d8947
richhh7g <richhh7g@protonmail.com>
2024-07-28 21:49:56 -0300

    feat: add documentation route

--- internal/app/api/config/http_route.config.go
+++ internal/app/api/config/http_route.config.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/richhh7g/term-alarms/internal/app/api/route"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
 type HTTPRouter struct {
@@ -23,6 +24,7 @@ func (c *HTTPRouter) Configure() (*chi.Mux, error) {
 	router.Use(middleware.DefaultLogger, middleware.RequestID, middleware.RealIP, middleware.Recoverer)
 
 	route.NewApi(router)
+	router.Get("/docs/*", httpSwagger.Handler())
 
 	return router, nil
 }
