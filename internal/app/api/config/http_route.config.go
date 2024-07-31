d47c4538fcf57deca99e76ef9c8b646c8309331c
richhh7g <richhh7g@protonmail.com>
2024-07-30 23:58:42 -0300

    chore: add cors

--- internal/app/api/config/http_route.config.go
+++ internal/app/api/config/http_route.config.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/richhh7g/term-alarms/internal/app/api/route"
 	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
@@ -21,7 +22,17 @@ func NewHTTPRouterConfig(ctx context.Context) *HTTPRouter {
 
 func (c *HTTPRouter) Configure() (*chi.Mux, error) {
 	router := chi.NewRouter()
-	router.Use(middleware.DefaultLogger, middleware.RequestID, middleware.RealIP, middleware.Recoverer)
+
+	corsMiddleware := cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	})
+
+	router.Use(corsMiddleware, middleware.DefaultLogger, middleware.RequestID, middleware.RealIP, middleware.Recoverer)
 
 	route.NewApi(router)
 	router.Get("/docs/*", httpSwagger.Handler())
