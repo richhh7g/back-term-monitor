e44e2731b84992209f13a1164eb3e69af7e1d5d0
richhh7g <richhh7g@protonmail.com>
2024-08-01 00:03:07 -0300

    feat: add check term endpoint

--- internal/app/api/route/api.route.go
+++ internal/app/api/route/api.route.go
@@ -1,10 +1,8 @@
 package route
 
 import (
-	"net/http"
-
 	"github.com/go-chi/chi/v5"
-	"github.com/richhh7g/term-alarms/internal/app/api/response"
+	"github.com/richhh7g/term-alarms/internal/app/api/handler"
 )
 
 type ApiImpl struct {
@@ -21,14 +19,10 @@ func NewApi(router *chi.Mux) {
 
 func (api *ApiImpl) RegisterRoutes() {
 	api.router.Route(api.v1Prefix(), func(r chi.Router) {
-		r.Get("/example", api.exampleHandler)
+		r.Post("/check-term", handler.NewCheckTermHandler)
 	})
 }
 
 func (api *ApiImpl) v1Prefix() string {
 	return "/api/v1"
-}
-
-func (api *ApiImpl) exampleHandler(w http.ResponseWriter, r *http.Request) {
-	response.NewSuccess(map[string]string{"hello": "world"}, 200).Send(w)
 }
