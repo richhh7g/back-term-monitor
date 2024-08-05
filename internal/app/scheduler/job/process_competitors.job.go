9866c7c38bb9b5a94622acfe0091885e6bc19828
richhh7g <richhh7g@protonmail.com>
2024-08-05 00:21:57 -0300

    feat: send pontential competitors email

--- internal/app/scheduler/job/process_competitors.job.go
+++ internal/app/scheduler/job/process_competitors.job.go
@@ -2,14 +2,18 @@ package schedule_job
 
 import (
 	"context"
+	"path"
 
 	competitor_usecase "github.com/richhh7g/back-term-monitor/internal/domain/usecase/competitor"
 	brand_datasource "github.com/richhh7g/back-term-monitor/internal/infra/data/brand"
+	resend_client "github.com/richhh7g/back-term-monitor/internal/infra/data/client/email/resend"
 	mongo_client "github.com/richhh7g/back-term-monitor/internal/infra/data/client/mongo"
 	mongo_repository "github.com/richhh7g/back-term-monitor/internal/infra/data/client/mongo/repository"
 	serpapi_client "github.com/richhh7g/back-term-monitor/internal/infra/data/client/serpapi"
 	competitor_datasource "github.com/richhh7g/back-term-monitor/internal/infra/data/competitor"
+	email_datasource "github.com/richhh7g/back-term-monitor/internal/infra/data/email"
 	"github.com/richhh7g/back-term-monitor/pkg/environment"
+	"github.com/richhh7g/back-term-monitor/pkg/localization"
 )
 
 type processCompetitorsImpl struct{}
@@ -35,6 +39,15 @@ func (j *processCompetitorsImpl) Run() {
 	competitorHttpDataSource := competitor_datasource.NewCompetitorHttpDataSource(client)
 	competitorDatasource := competitor_datasource.NewCompetitorDataSource(competitorHttpDataSource, competitorDBDataSource)
 
-	processCompetitorsUseCase := competitor_usecase.NewProcessCompetitorsUseCase(brandDataSource, competitorDatasource)
+	localizationService := localization.NewLocalization()
+	localizationService.AddLanguages(map[localization.Language]string{
+		localization.EN_US: path.Join("pkg", "localization", "locale", "en_us.locale.yml"),
+		localization.PT_BR: path.Join("pkg", "localization", "locale", "pt_br.locale.yml"),
+	})
+
+	resendClient := resend_client.NewResend()
+	emailDataSource := email_datasource.NewEmailDataSource(resendClient, localizationService)
+
+	processCompetitorsUseCase := competitor_usecase.NewProcessCompetitorsUseCase(brandDataSource, competitorDatasource, emailDataSource)
 	processCompetitorsUseCase.Exec(ctx)
 }
